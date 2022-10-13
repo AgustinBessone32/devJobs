25cc6ac30b632260440f0e0b7c2bfc8678cd7ced
Agustin Bessone <bessone98@gmail.com>
2022-10-13 19:49:00 -0300

    agregando model de vacante con su formulario

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const homeController = require('../controllers/homeController')
+const vacantesController = require('../controllers/vacanteController')
+
 const router = express.Router()
 
 module.exports = () =>{
     router.get('/',homeController.mostrarTrabajos )
+
+    router.get('/vacantes/nueva', vacantesController.formularioNuevaVacante)
 
     return router
 }
